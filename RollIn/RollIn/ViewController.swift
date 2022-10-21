0100a5278a7f0d83195cf262fe2b424c0db2cb31
feldblume5263 <feldblume5263@gmail.com>
2022-10-21 14:28:22 +0900

    init xcode project

---
+++ RollIn/RollIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RollIn
+//
+//  Created by Noah Park on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
