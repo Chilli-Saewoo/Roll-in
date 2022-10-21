19c490520d3434b1d5ba5456540a48038becbed9
feldblume5263 <feldblume5263@gmail.com>
2022-10-21 22:28:29 +0900

    feat: #1 Nickname setting 화면 생성
    ViewController을 생성했습니다.
    아직 스토리보드와는 연결되지 않은 상태입니다.

---
+++ RollIn/RollIn/Screens/NicknameSetting/NicknameSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NicknameSettingViewController.swift
+//  RollIn
+//
+//  Created by Noah Park on 2022/10/21.
+//
+
+import UIKit
+
+class NicknameSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
