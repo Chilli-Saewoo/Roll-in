df669cebba0b0b8571789fceac292a950a058e56
Taekhwan Han <hth3396@gmail.com>
2022-10-22 22:33:06 +0900

    feat: #2 UITableViewDataSource 추가 받아올 Rollingpaper Data 를 반영할 UITableViewDataSource 추가

--- RollIn/RollIn/Screens/Rollingpaper/RollingpaperViewController.swift
+++ RollIn/RollIn/Screens/Rollingpaper/RollingpaperViewController.swift
@@ -19,3 +19,15 @@ final class RollingpaperViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+extension RollingpaperViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "RollingpaperCell") as? RollingpaperTableViewCell ?? UITableViewCell()
+        
+        return cell
+    }
+}
