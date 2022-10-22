2fef2bf36f7e3c239e68080126cfe0375ab3b038
Taekhwan Han <hth3396@gmail.com>
2022-10-22 21:48:10 +0900

    feat: #2 롤링페이퍼 작성자 레이블 UI 구현 TableViewCell 구성요소 중 하나인 작성자 레이블 UI 구현

--- RollIn/RollIn/Screens/Rollingpaper/Views/RollingpaperTableViewCell.swift
+++ RollIn/RollIn/Screens/Rollingpaper/Views/RollingpaperTableViewCell.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 final class RollingpaperTableViewCell: UITableViewCell {
+    public lazy var writerNickname: UILabel = UILabel(frame: .zero)
+    public lazy var message: UILabel = UILabel(frame: .zero)
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension RollingpaperTableViewCell {
+    func setUpView() {
+        setWriterNickname()
+    }
+    
+    func setWriterNickname() {
+        self.contentView.addSubview(writerNickname)
+        writerNickname.translatesAutoresizingMaskIntoConstraints = false
+        writerNickname.font = .preferredFont(forTextStyle: .body)
+        writerNickname.textColor = .black
+        NSLayoutConstraint.activate([
+            writerNickname.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
+            writerNickname.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            writerNickname.widthAnchor.constraint(equalToConstant: 100),
+            writerNickname.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func setMessage() {
+        
+    }
 }
