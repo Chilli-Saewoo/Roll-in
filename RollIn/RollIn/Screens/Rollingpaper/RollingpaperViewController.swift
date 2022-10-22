517ed2bc17c05b58da1ab91be35f35ef267bc24c
Taekhwan Han <hth3396@gmail.com>
2022-10-22 21:46:42 +0900

    feat: #2 롤링페이퍼 보여줄 임시 테이블뷰 디렉토리 설정 ViewController 생성 TableViewCell 생성

---
+++ RollIn/RollIn/Screens/Rollingpaper/RollingpaperViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RollingpaperViewController.swift
+//  RollIn
+//
+//  Created by 한택환 on 2022/10/22.
+//
+
+import UIKit
+
+final class RollingpaperViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RollIn/RollIn/Screens/Rollingpaper/Views/RollingpaperTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RollingpaperTableViewCell.swift
+//  RollIn
+//
+//  Created by 한택환 on 2022/10/22.
+//
+
+import UIKit
+
+final class RollingpaperTableViewCell: UITableViewCell {
+    
+}
