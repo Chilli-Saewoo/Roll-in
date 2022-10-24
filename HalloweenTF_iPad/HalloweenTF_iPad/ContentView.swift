099e7684725633a861960d33cf5f95207e2c7a8e
sherry <yeahaluu@gmail.com>
2022-10-24 17:08:29 +0900

    init project (HalloweenTF_iPad)

---
+++ HalloweenTF_iPad/HalloweenTF_iPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HalloweenTF_iPad
+//
+//  Created by Yeji on 2022/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
