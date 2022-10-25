5e3b31a11ee84b0a9d12a2fd7294a50153cf8bb5
sherry <yeahaluu@gmail.com>
2022-10-25 18:35:49 +0900

    feat: #21 iPAD에서firebase의 변경사항 추적

--- HalloweenTF_iPad/HalloweenTF_iPad/HalloweenTF_iPadApp.swift
+++ HalloweenTF_iPad/HalloweenTF_iPad/HalloweenTF_iPadApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct HalloweenTF_iPadApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
