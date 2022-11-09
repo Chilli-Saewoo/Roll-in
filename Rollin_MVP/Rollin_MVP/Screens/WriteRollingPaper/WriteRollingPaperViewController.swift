5c6a99727178a24f9797b1bba4c1a92fe0007662
Nick(Seungyun Kim) <sy5072@naver.com>
2022-11-09 15:43:38 +0900

    [feat] Privacy switch 레이아웃 구현 (#48)

--- Rollin_MVP/Rollin_MVP/Screens/WriteRollingPaper/WriteRollingPaperViewController.swift
+++ Rollin_MVP/Rollin_MVP/Screens/WriteRollingPaper/WriteRollingPaperViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 final class WriteRollingPaperViewController: UIViewController {
     
     private let postThemePicerkView = PostThemePickerView()
+    private let privateSwitch: UISwitch = {
+        let privateSwitch = UISwitch()
+        return privateSwitch
+    }()
+    
+    private let privateSwitchTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "그룹내 공개"
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +34,20 @@ final class WriteRollingPaperViewController: UIViewController {
             postThemePicerkView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             postThemePicerkView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             postThemePicerkView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        view.addSubview(privateSwitch)
+        privateSwitch.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            privateSwitch.topAnchor.constraint(equalTo: postThemePicerkView.bottomAnchor, constant: 8),
+            privateSwitch.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+        ])
+        
+        view.addSubview(privateSwitchTitleLabel)
+        privateSwitchTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            privateSwitchTitleLabel.centerYAnchor.constraint(equalTo: privateSwitch.centerYAnchor),
+            privateSwitchTitleLabel.trailingAnchor.constraint(equalTo: privateSwitch.leadingAnchor, constant: -8)
         ])
     }
 }
