0c1c73a76477734c98ffbae0cc5065ef00430258
Nick(Seungyun Kim) <sy5072@naver.com>
2022-11-10 20:05:42 +0900

    [feat] Color Asset 추가 (#65)

---
+++ Rollin_MVP/Rollin_MVP/Global/Extension+/UIColor+.swift
@@ -0,0 +1,39 @@
+//
+//  UIColor+.swift
+//  Rollin_MVP
+//
+//  Created by Seungyun Kim on 2022/11/10.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    //MARK: Background Color
+    static let inactiveBgGray = UIColor(named: "inactiveBgGray")
+    
+    static let bgBlue = UIColor(named: "bgBlue")
+    
+    static let bgGreen = UIColor(named: "bgGreen")
+    
+    static let bgPurple = UIColor(named: "bgPurple")
+    
+    static let bgRed = UIColor(named: "bgRed")
+    
+    static let bgYellow = UIColor(named: "bgYellow")
+    
+    //MARK: Text Color
+    static let systemBlack = UIColor(named: "systemBlack")
+    
+    static let inactiveTextGray = UIColor(named: "inactiveTextGray")
+    
+    static let textBlue = UIColor(named: "textBlue")
+    
+    static let textGreen = UIColor(named: "textGreen")
+    
+    static let textPurple = UIColor(named: "textPurple")
+    
+    static let textRed = UIColor(named: "textRed")
+    
+    static let textYellow = UIColor(named: "textYellow")
+}
