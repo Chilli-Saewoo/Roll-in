e4a6514932df57b3de950641807a3140420341d1
Nick(Seungyun Kim) <sy5072@naver.com>
2022-11-11 19:12:21 +0900

    [feat] hide keyboard when tapped around  기능 extension 추가 (#69)

---
+++ Rollin_MVP/Rollin_MVP/Global/Extension+/UIViewController+.swift
@@ -0,0 +1,20 @@
+//
+//  UIController+.swift
+//  Rollin_MVP
+//
+//  Created by Seungyun Kim on 2022/11/11.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
