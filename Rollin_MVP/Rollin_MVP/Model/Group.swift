2490f709c7b1ba93fb3b4ed0873fe92c0ae0328f
feldblume5263 <feldblume5263@gmail.com>
2022-11-13 09:42:08 +0900

    feat: #86 메인화면 DB 받아오기
    받아오기 완료

---
+++ Rollin_MVP/Rollin_MVP/Model/Group.swift
@@ -0,0 +1,40 @@
+//
+//  Group.swift
+//  Rollin_MVP
+//
+//  Created by Noah Park on 2022/11/13.
+//
+
+import UIKit
+
+// code: "ASDFS"
+// groupName: "칠리새우"
+// groupTheme: "red"
+// groupIcon: "school"
+// timestamp: 1234555666666
+
+final class Group: Codable, Comparable {
+    let groupName: String
+    let groupTheme: String
+    let groupIcon: String
+    let code: String
+    let timestamp: Date
+    var participants: [(String, String)]?
+    
+    enum CodingKeys: String, CodingKey {
+        case groupName
+        case groupTheme
+        case groupIcon
+        case code
+        case timestamp
+    }
+    
+    static func < (lhs: Group, rhs: Group) -> Bool {
+        lhs.timestamp < rhs.timestamp
+    }
+    
+    static func == (lhs: Group, rhs: Group) -> Bool {
+        lhs.code == rhs.code
+    }
+    
+}
--- Rollin_MVP/Rollin_MVP/Screens/AppMain/MainViewController.swift
+++ Rollin_MVP/Rollin_MVP/Screens/AppMain/MainViewController.swift
@@ -6,17 +6,62 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 final class MainViewController: UIViewController {
-    
+    private let db = Firestore.firestore()
     private let mainTitleLabel = UILabel()
     private let addGroupCard = AddGroupButtonBackgroundView()
+    private var groups: [Group] = [] {
+        didSet {
+            print(groups)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setMainTitleLabel()
         setAddGroupCard()
         addGroupCard.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let uid = UserDefaults.standard.string(forKey: "uid") {
+            let docRef = db.collection("userGroups").document(uid)
+            docRef.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let groupIds = document.data().map{
+                        $0["division"] as? [String] ?? []
+                    } ?? [String]()
+                    self.groups.removeAll()
+                    for groupId in groupIds {
+                        let groupsRef = self.db.collection("groups").document(groupId)
+                        groupsRef.getDocument(as: Group.self) { result in
+                            switch result {
+                                case .success(let group):
+                                self.db.collection("groupUsers").document(groupId).collection("participants").getDocuments() { querySnapshot, error in
+                                    if let err = error {
+                                        print("Error getting documents: \(err)")
+                                    } else {
+                                        for document in querySnapshot?.documents ?? [] {
+                                            group.participants?.append((document.documentID, document.data()["groupNickname"] as? String ?? ""))
+                                        }
+                                        self.groups.append(group)
+                                    }
+                                }
+                                case .failure(let error):
+                                print("Error decoding group: \(error)")
+                            }
+                        }
+                    }
+                    
+                    
+                } else {
+                    print("Document does not exist")
+                }
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
