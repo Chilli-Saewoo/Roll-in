b0cb35bf5b3234a9d9fa2f4bf245b2224bf7e209
Haru <kkang47140@gmail.com>
2022-11-13 15:45:07 +0900

    develop merge

---
+++ Rollin_MVP/Rollin_MVP/Global/Extension+/NSTextAttachment+.swift
@@ -0,0 +1,16 @@
+//
+//  NSTextAttachment+.swift
+//  Rollin_MVP
+//
+//  Created by Noah Park on 2022/11/13.
+//
+import UIKit
+
+extension NSTextAttachment {
+    func setImageHeight(height: CGFloat) {
+        guard let image = image else { return }
+        let ratio = image.size.width / image.size.height
+
+        bounds = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: ratio * height, height: height)
+    }
+}
