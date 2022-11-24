83ed7ed80f17544ea15c928c9803a67a117163c3
Taekhwan Han <hth3396@gmail.com>
2022-11-24 11:30:13 +0900

    feat: #116 버튼 공통 컴포넌트 추가 다음, 추가하기, 완료의 공통 UIButton 컴포넌트를 GroupBaseVC에 추

--- Rollin_MVP/Rollin_MVP/Screens/Base/GroupBaseViewController.swift
+++ Rollin_MVP/Rollin_MVP/Screens/Base/GroupBaseViewController.swift
@@ -5,4 +5,38 @@
 //  Created by 한택환 on 2022/11/23.
 //
 
-import Foundation
+import UIKit
+
+final class GroupBaseViewController: UIViewController {
+    
+    lazy var confirmButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension GroupBaseViewController {
+    
+    func setLayout() {
+        //Override Layout
+    }
+    
+    func setConfirmButton(buttonTitle: String) {
+        view.addSubview(confirmButton)
+        confirmButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            confirmButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -34),
+            confirmButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            confirmButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            confirmButton.heightAnchor.constraint(equalToConstant: 56),
+        ])
+        confirmButton.setTitle(buttonTitle, for: .normal)
+        confirmButton.layer.cornerRadius = 4.0
+        confirmButton.backgroundColor = .systemBlack
+    }
+}
