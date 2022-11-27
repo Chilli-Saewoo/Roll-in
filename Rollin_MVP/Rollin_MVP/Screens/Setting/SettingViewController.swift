8391f07766b12330bbdec29bc2c6d9c71b786c79
Nick(Seungyun Kim) <sy5072@naver.com>
2022-11-27 15:39:18 +0900

    [add] Setting View Controller 추가 (#131)

---
+++ Rollin_MVP/Rollin_MVP/Screens/Setting/SettingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingViewController.swift
+//  Rollin_MVP
+//
+//  Created by Seungyun Kim on 2022/11/27.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+    
+}
