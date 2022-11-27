8a1f9d7931d6689d2399a32eaec7011185e41d13
Nick(Seungyun Kim) <sy5072@naver.com>
2022-11-27 15:54:22 +0900

    [feat] Setting View tableview cell 구현 (#131)

---
+++ Rollin_MVP/Rollin_MVP/Screens/Setting/ViewComponents/SettingTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  SettingTableViewCell.swift
+//  Rollin_MVP
+//
+//  Created by Seungyun Kim on 2022/11/27.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+    private let settingTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    private let navigationArrowImageView: UIImageView = {
+        let image = UIImageView(image: UIImage(systemName: "chevron.forward"))
+        image.tintColor = .systemBlack
+        return image
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    private func setupLayout(){
+        addSubview(settingTitleLabel)
+        settingTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            settingTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            settingTitleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+        
+        addSubview(navigationArrowImageView)
+        navigationArrowImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            navigationArrowImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            navigationArrowImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+        ])
+    }
+    
+    func configureUI(title: String, isArrowHidden: Bool) {
+        self.settingTitleLabel.text = title
+        self.navigationArrowImageView.isHidden = isArrowHidden
+    }
+}
