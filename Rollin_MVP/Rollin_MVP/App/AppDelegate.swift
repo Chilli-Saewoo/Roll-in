a9a294df9efaa81d103e987d95142b574fc3fe40
lee02029 <kkang47140@gmail.com>
2022-11-29 02:06:47 +0900

    로그아웃 및 회원탈퇴 기능 추가

--- Rollin_MVP/Rollin_MVP/App/AppDelegate.swift
+++ Rollin_MVP/Rollin_MVP/App/AppDelegate.swift
@@ -12,8 +12,6 @@ import FirebaseFirestore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         let db = Firestore.firestore()
--- Rollin_MVP/Rollin_MVP/Screens/Setting/SettingViewController.swift
+++ Rollin_MVP/Rollin_MVP/Screens/Setting/SettingViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 final class SettingViewController: UIViewController {
     
@@ -17,6 +19,8 @@ final class SettingViewController: UIViewController {
         case logout = "로그아웃"
         case signout = "회원탈퇴"
     }
+    
+    private let db = Firestore.firestore()
     
     private let settingTitleLabel: UILabel = {
         let label = UILabel()
@@ -30,7 +34,7 @@ final class SettingViewController: UIViewController {
         tableView.rowHeight = 60
         return tableView
     }()
-
+    
     
     
     private let settingTitleList: [SettingTitle] = [.setNickname, .privacyPolicy, .openLicense, .mailToDeveloper, .logout, .signout]
@@ -69,7 +73,7 @@ final class SettingViewController: UIViewController {
 }
 
 extension SettingViewController: UITableViewDelegate {
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         settingTableView.deselectRow(at: indexPath, animated: true)
         
@@ -90,11 +94,90 @@ extension SettingViewController: UITableViewDelegate {
             print("mailToDeveloper")
         case .logout:
             //TODO: 추후 로그아웃 들어갈 예정
-            print("logout")
-        case .signout: break
+            let alert = UIAlertController(title: "로그아웃 하시겠습니까?", message: nil, preferredStyle: .alert)
+            let action = UIAlertAction(title: "로그아웃하기", style: .destructive, handler: {_ in
+                try! FirebaseAuth.Auth.auth().signOut()
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+                let navigationController = UINavigationController(rootViewController: vc)
+                let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+                guard let delegate = sceneDelegate else {
+                    return
+                }
+                delegate.window?.rootViewController = navigationController
+            })
+            let cancel = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+            alert.addAction(cancel)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        case .signout:
             //TODO: 추후 회원탈퇴 로직 들어갈 예정
-            }
+            //              users에 해당하는 user_uid값을 체크
+            let alert = UIAlertController(title: "회원탈퇴 하시겠습니까?", message: nil, preferredStyle: .alert)
+            let action = UIAlertAction(title: "회원탈퇴하기", style: .destructive, handler: {_ in
+                if let uid = UserDefaults.standard.string(forKey: "uid") {
+                    //                 userGroups에 해당하는 user_uid값을 체크하고 현재 참여중인 그룹을 확인
+                    let docRef = self.db.collection("userGroups").document(uid)
+                    docRef.getDocument { (document, error) in
+                        if let document = document, document.exists {
+                            let groupIds = document.data().map{
+                                $0["division"] as? [String] ?? []
+                            } ?? [String]()
+                            for idx in 0..<groupIds.count {
+                                let groupsRef = self.db.collection("groups").document(groupIds[idx])
+                                groupsRef.getDocument(as: Group.self) { result in
+                                    switch result {
+                                    case .success(let group):
+                                        //                                  그 뒤에 GroupUsers에서 해당하는 그룹 group_UUID를 체크 한 후 participants에 들어가서 해당하는 user_uid값 삭제
+                                        self.db.collection("groupUsers").document(groupIds[idx]).collection("participants").getDocuments() { querySnapshot, error in
+                                            if let err = error {
+                                                print("Error getting documents: \(err)")
+                                            } else {
+                                                group.participants = []
+                                                for _ in querySnapshot?.documents ?? [] {
+                                                    self.db.collection("groupUsers").document(groupIds[idx]).collection("participants").document(UserDefaults.standard.string(forKey: "uid") ?? "").delete()
+                                                    self.db.collection("groupUsers").document(groupIds[idx]).collection("participants").getDocuments { querySnapshot, error in
+                                                        if let err = error {
+                                                            print("Error getting documents: \(err)")
+                                                        } else {
+                                                            guard let docs = querySnapshot?.documents else { return }
+                                                            if docs.count == 0 {
+                                                                self.db.collection("groupUsers").document(groupIds[idx]).delete()
+                                                            }
+                                                        }
+                                                    }
+                                                    
+                                                }
+                                                self.db.collection("userGroups").document(UserDefaults.standard.string(forKey: "uid") ?? "").delete()
+                                                self.db.collection("users").document(UserDefaults.standard.string(forKey: "uid") ?? "").delete()
+                                                UserDefaults.standard.removeObject(forKey: "uid")
+                                            }
+                                        }
+                                    case .failure(let error):
+                                        print("Error decoding group: \(error)")
+                                    }
+                                }
+                            }
+                        } else {
+                            print("Document does not exist")
+                        }
+                    }
+                }
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+                let navigationController = UINavigationController(rootViewController: vc)
+                let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+                guard let delegate = sceneDelegate else {
+                    return
+                }
+                delegate.window?.rootViewController = navigationController
+            })
+            let cancel = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+            alert.addAction(cancel)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
         }
+    }
 }
 
 extension SettingViewController: UITableViewDataSource {
