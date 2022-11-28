27af14b18fac330315e3a3d1716ca9a858fa13e8
Taekhwan Han <hth3396@gmail.com>
2022-11-28 14:58:19 +0900

    feat: #138 AppInfoWebVC 에 WKWebView 연결 임시 노션 개인정보 보호정책 페이지를 연결했습니다

--- Rollin_MVP/Rollin_MVP/Screens/Setting/AppInfoWebViewController.swift
+++ Rollin_MVP/Rollin_MVP/Screens/Setting/AppInfoWebViewController.swift
@@ -6,9 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
 final class AppInfoWebViewController: UIViewController {
+    var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        let myURL = URL(string:"https://mercury-comte-8e6.notion.site/ea8b9c2b9cfd469da4c70285362e6a28")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        setWebView()
+    }
+}
+
+private extension AppInfoWebViewController {
+    func setWebView() {
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            webView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
     }
 }
